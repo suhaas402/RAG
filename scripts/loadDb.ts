623bb2a1ef953db4c9882689a38d6b7208bce80d
suhaas402 <suhaas402@gmail.com>
2025-03-10 23:25:33 +0530

    commit1

---
+++ scripts/loadDb.ts
@@ -0,0 +1,10 @@
+import {DataAPIClient} from "@datastax/astra-db-ts"
+import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web";
+
+
+
+import ollama from "ollama"
+
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
+
+import"dotenv/config"
