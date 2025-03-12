30a3a9cb814be7cdce59e678ad796e230ca5e25d
sumukh malvey <sumukhmalvey@sumukhs-MacBook-Air.local>
2025-03-13 02:56:54 +0530

    few changes

--- scripts/loadDb.ts
+++ scripts/loadDb.ts
@@ -1,10 +1,6 @@
-import {DataAPIClient} from "@datastax/astra-db-ts"
-import { PuppeteerWebBaseLoader } from "langchain/document_loaders/web";
-
-
-
-import ollama from "ollama"
-
-import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
-
 import"dotenv/config"
+import {DataAPIClient} from "@datastax/astra-db-ts"
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { chromium, firefox, webkit } from 'playwright';
+import ollama from "ollama"
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter"
